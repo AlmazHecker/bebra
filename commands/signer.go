c3b9648e40ad2aee8f9d305dfd4073fd8cd28eec
Almaz's Zefirka <almazbek.sheraliev321@gmail.com>
2024-12-07 23:28:09 +0600

    bugFix(apk-signer): fix sign issue

--- commands/signer.go
+++ commands/signer.go
@@ -9,43 +9,42 @@ import (
 )
 
 var signerCmd = &cobra.Command{
-    Use:   "signer [dir with apk]",
-    Short: "A tool to sign APK files",
-    Args: cobra.ExactArgs(1),
-    Run: signerHandler,
+	Use:   "signer [dir with apk]",
+	Short: "A tool to sign APK files",
+	Args:  cobra.ExactArgs(1),
+	Run:   signerHandler,
 }
 
 func signerHandler(cmd *cobra.Command, args []string) {
 	if !helpers.DirExists(args[0]) {
 		fmt.Printf("The given file(%s) not found!\n", args[0])
-        os.Exit(1)
+		os.Exit(1)
 	}
 
-    output, _ := cmd.Flags().GetString("output")
-    keystore, _ := cmd.Flags().GetString("keystore")
-    pass, _ := cmd.Flags().GetString("keystore")
+	output, _ := cmd.Flags().GetString("output")
+	keystore, _ := cmd.Flags().GetString("keystore")
+	pass, _ := cmd.Flags().GetString("pass")
 
-    if !helpers.FileExists(keystore) {
-        helpers.ErrorLog("The keystore file not found!")
-        os.Exit(1)
-    }
+	if !helpers.FileExists(keystore) {
+		helpers.ErrorLog("The keystore file not found!")
+		os.Exit(1)
+	}
 
-    if err := helpers.Signer(args[0], output, keystore, BebraConfig.Signer, pass); err != nil {
-        helpers.ErrorLog(fmt.Sprintf("Error signing APK: %v", err))
-        os.Exit(1)
-    }
+	if err := helpers.Signer(args[0], output, keystore, BebraConfig.Signer, pass); err != nil {
+		helpers.ErrorLog(fmt.Sprintf("Error signing APK: %v", err))
+		os.Exit(1)
+	}
 
-    fmt.Println("APK signed successfully:", output)
+	fmt.Println("APK signed successfully:", output)
 
 }
 
-
 func init() {
-    signerCmd.Flags().StringP("keystore", "k", "", "Path to the keystore (JKS file)")
-    signerCmd.Flags().StringP("pass", "p", "", "keystore password")
-    signerCmd.Flags().StringP("output", "o", "./signed_apks", "Path for the output signed APK files")
+	signerCmd.Flags().StringP("keystore", "k", "", "Path to the keystore (JKS file)")
+	signerCmd.Flags().StringP("pass", "p", "", "keystore password")
+	signerCmd.Flags().StringP("output", "o", "./signed_apks", "Path for the output signed APK files")
 
-    signerCmd.MarkFlagRequired("k")
-    signerCmd.MarkFlagRequired("p")
+	signerCmd.MarkFlagRequired("k")
+	signerCmd.MarkFlagRequired("p")
 
 }
--- helpers/signer.go
+++ helpers/signer.go
@@ -8,39 +8,38 @@ import (
 )
 
 func Signer(inputFolder string, outputFolder string, keystore string, signer string, pass string) error {
-    files, err := filepath.Glob(filepath.Join(inputFolder, "*.apk"))
-    if err != nil {
-        fmt.Printf("Error reading APK files: %s\n", err)
-        os.Exit(1)
-    }
+	files, err := filepath.Glob(filepath.Join(inputFolder, "*.apk"))
+	if err != nil {
+		fmt.Printf("Error reading APK files: %s\n", err)
+		os.Exit(1)
+	}
 
-    for _, file := range files {
-        fmt.Printf("Signing %s...\n", file)
+	for _, file := range files {
+		fmt.Printf("Signing %s...\n", file)
 
-        outputFile := filepath.Join(outputFolder, filepath.Base(file))
+		outputFile := filepath.Join(outputFolder, filepath.Base(file))
 
-        fmt.Println(file, outputFile, keystore)
+		fmt.Println(file, outputFile, keystore)
 		execCmd := signerCmd(file, outputFile, keystore, signer, pass)
 
-        execCmd.Stdin = os.Stdin
-        execCmd.Stdout = os.Stdout
-        execCmd.Stderr = os.Stderr
+		execCmd.Stdin = os.Stdin
+		execCmd.Stdout = os.Stdout
+		execCmd.Stderr = os.Stderr
 
 		if err := execCmd.Run(); err != nil {
-            fmt.Printf("Failed to sign %s: %s\n", file, err)
-            os.Exit(1)
-        }
-    }
+			fmt.Printf("Failed to sign %s: %s\n", file, err)
+			os.Exit(1)
+		}
+	}
 
-    return err;
+	return err
 }
 
 func signerCmd(file string, outputFile string, keystore string, signer string, pass string) *exec.Cmd {
-    outputDir := filepath.Dir(outputFile)
-    if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
-        fmt.Printf("Failed to create output directory: %s\n", err)
-       return nil
-    }
-
-	return exec.Command(signer, "sign", "--ks", keystore, "--ks-pass", "pass:" + pass, "--out", outputFile, file)
+	outputDir := filepath.Dir(outputFile)
+	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
+		fmt.Printf("Failed to create output directory: %s\n", err)
+		return nil
+	}
+	return exec.Command(signer, "sign", "--ks", keystore, "--ks-pass", "pass:"+pass, "--out", outputFile, file)
 }
